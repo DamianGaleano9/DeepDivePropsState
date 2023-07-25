4b3c7c0e6f155c0aab2eca22ae519d23fb05e25b
Damian Galeano <dj.damian.b@hotmail.com>
2023-07-25 23:47:10 +0200

    Add JournalList Component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import JournalList from "./journal/journal_list";
+
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>React, Props, and State Deep Dive</h1>
+        <h2><JournalList/></h2>
       </div>
     );
   }
